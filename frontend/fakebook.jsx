ccee87db9022a9705e6497b67b2dbd2595b10e3e
David Anderson <dlande000@gmail.com>
2019-01-22 14:09:06 -0500

    Add preloaded state and current user to entry file

--- frontend/fakebook.jsx
+++ frontend/fakebook.jsx
@@ -3,5 +3,13 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  let preloadedState = undefined; 
+  if (window.currentUser) {
+    preloadedState = {
+      session: {
+        currentUser: window.currentUser
+      }
+    };
+  }
   ReactDOM.render(<h1>Welcome to Fakebook</h1>, root);
 });
