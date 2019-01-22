bc9d2f7df53ab21458a9bb4fba76902a3310732f
David Anderson <dlande000@gmail.com>
2019-01-22 12:16:21 -0500

    Complete frontend/actions/session.js

---
+++ frontend/actions/session.js
@@ -0,0 +1,22 @@
+import * as SessionApiUtil from '../util/session';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+
+const receiveCurrentUser = user => ({
+    type: RECEIVE_CURRENT_USER,
+    user
+});
+
+const logoutCurrentUser = () => ({
+    type: LOGOUT_CURRENT_USER
+});
+
+export const createNewUser = formUser => dispatch => SessionApiUtil.postUser(formUser)
+    .then(user => dispatch(receiveCurrentUser(user)));
+
+export const login = formUser => dispatch => SessionApiUtil.postSession(formUser)
+    .then(user => dispatch(receiveCurrentUser(user)));
+
+export const logout = () => dispatch => SessionApiUtil.deleteSession()
+    .then(() => dispatch(logoutCurrentUser()));
