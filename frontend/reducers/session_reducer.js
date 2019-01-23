4275b834ed06b3ea3f121b2ae317434d7ae57f10
David Anderson <dlande000@gmail.com>
2019-01-23 09:23:05 -0500

    Add session reducer

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,16 +1,16 @@
 import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session_actions';
   
-  const _nullUser = Object.freeze({
-    id: null
-  });
+  const _nullSession = {
+      currentUser: null
+  };
   
-  const sessionReducer = (state = _nullUser, action) => {
+  const sessionReducer = (state = _nullSession, action) => {
     Object.freeze(state);
     switch(action.type) {
       case RECEIVE_CURRENT_USER:
         return { id: action.currentUser.id };
       case LOGOUT_CURRENT_USER:
-        return _nullUser;
+        return _nullSession;
       default:
         return state;
     }
