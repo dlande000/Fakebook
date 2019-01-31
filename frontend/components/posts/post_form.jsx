bdf9b19755d397b37ae83761111581415ce73a14
David Anderson <dlande000@gmail.com>
2019-01-31 18:18:29 -0500

    Skeleton for post form

--- frontend/components/posts/post_form.jsx
+++ frontend/components/posts/post_form.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 
 class PostForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          body: '',
+          receiverId: ""
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+    }
+
+    update(body) {
+        return e => this.setState({
+          [body]: e.target.value
+        });
+    }
+
     render() {
     return (
         <div>
             <h1>This is the psot form</h1>
+            <form onSubmit={this.handleSubmit}>
+            <textarea name="" id="" cols="30" rows="10" onChange={this.update('body')}></textarea>
+            <input type="submit" value="Post" />
+            </form>
         </div>
         )
     }
