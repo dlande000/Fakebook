06a071e4a0a074ed80d9c719baffca0266b99f6e
David Anderson <dlande000@gmail.com>
2019-01-31 11:23:40 -0500

    Add posts errors reducer, update errors reducer

---
+++ frontend/reducers/posts_errors_reducer.js
@@ -0,0 +1,17 @@
+import { RECEIVE_POST_ERRORS, RECEIVE_POSTS, REMOVE_POST } from '../actions/posts_actions';
+  
+const postsErrorsReducer = (state = [], action) => {
+    Object.freeze(state);
+    switch (action.type) {
+      case RECEIVE_POST_ERRORS:
+        return action.errors;
+      case RECEIVE_POSTS:
+        return [];
+      case REMOVE_POST:
+        return [];
+      default:
+        return state;
+    }
+};
+
+export default postsErrorsReducer;
