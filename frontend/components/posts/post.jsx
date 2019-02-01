8702f29db46d2e314f87c848cc1c2e96d284be62
David Anderson <dlande000@gmail.com>
2019-02-01 09:05:44 -0500

    CSS changes

--- frontend/components/posts/post.jsx
+++ frontend/components/posts/post.jsx
@@ -6,16 +6,19 @@ class Post extends React.Component {
     const receiver = this.props.users[this.props.post.receiverId];
     const author = this.props.users[this.props.post.authorId];
 
+    let authorName = `${author.first_name} ${author.last_name}`;
+    let receiverName = `${receiver.first_name} ${receiver.last_name}`
+
     let authorAndReceiver;
     if (author.id === receiver.id) {
-        authorAndReceiver = `${author.first_name} ${author.last_name}`;
+        authorAndReceiver = () => (<h6 className="post-authors">{authorName}</h6>);
     } else {
-        authorAndReceiver = `${author.first_name} ${author.last_name} ${receiver.first_name} ${receiver.last_name}`;
+        authorAndReceiver = () => (<h6 className="post-authors">{authorName}   <b>&#9658;</b>   {receiverName}</h6>);
     }
 
     let createdAndEdited;
     if (this.props.createdAt === this.props.updatedAt) {
-        createdAndEdited = `${new Date(this.props.post.createdAt)}`;
+        createdAndEdited = `${new Date(this.props.post.createdAt).toDateString()}`;
     } else {
         createdAndEdited = `Created at ${this.props.post.createdAt}; edited at ${this.props.post.updatedAt}`;
     }
@@ -25,7 +28,7 @@ class Post extends React.Component {
             <div className="post-image-container">
                 <img className="post-image" src={author.profile_pic_url}/>
             </div>
-            <h6 className="post-authors">{authorAndReceiver}</h6>
+            {authorAndReceiver()}
             <p className="post-created">{createdAndEdited}</p>
             <p className="post-body">{this.props.post.body}</p>
         </div>
--- frontend/components/posts/post_form.jsx
+++ frontend/components/posts/post_form.jsx
@@ -24,8 +24,10 @@ class PostForm extends React.Component {
     render() {
     return (
         <div className="post-form">
+        <div className="post-form-top">
             <img className="pencil-post-image" src="https://image.flaticon.com/icons/svg/39/39681.svg"/><h6 className="post-form-header">Create Post</h6>
-            <form onSubmit={this.handleSubmit}>
+            </div>
+            <form className="form" onSubmit={this.handleSubmit}>
             <textarea className="post-form-body" onChange={this.update('body')} placeholder="Write a post ..." value={this.state.body}></textarea>
             <input className="post-form-submit-button" type="submit" value="Post" />
             </form>
