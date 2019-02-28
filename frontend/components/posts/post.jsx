4b203637072340ad8bd4c4269178913f0992a91d
David Anderson <dlande000@gmail.com>
2019-02-28 17:13:47 -0500

    Remove extra files

--- frontend/components/posts/post.jsx
+++ frontend/components/posts/post.jsx
@@ -7,7 +7,7 @@ class Post extends React.Component {
     const receiver = this.props.users[this.props.post.receiverId];
     const author = this.props.users[this.props.post.authorId];
     let authorName = `${author.first_name} ${author.last_name}`;
-    let receiverName = `${receiver.first_name} ${receiver.last_name}`
+    let receiverName = `${receiver.first_name} ${receiver.last_name}`;
 
     let authorAndReceiver;
     if (author.id === receiver.id) {
