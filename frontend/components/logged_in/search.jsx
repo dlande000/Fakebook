72a1a02002f2bbaa099b281bae2549853f4fdaa9
David Anderson <dlande000@gmail.com>
2019-03-01 14:23:16 -0500

    Add file for search suggestions

--- frontend/components/logged_in/search.jsx
+++ frontend/components/logged_in/search.jsx
@@ -1,21 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Suggestions from './suggestions';
 
 class Search extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            searchTerm: '',
-            currentlyDisplayed: this.props.users
-        };
+  this.state = {
+    query: '',
+    results: []
+  };
 
-        this.onInputChange = this.onInputChange.bind(this);
-    }
-
-    onInputChange(event) {
-    }
-
-    render() {}
+  this.handleInputChange = this.handleInputChange.bind(this);
+  this.getInfo = this.getInfo.bind(this);
 }
 
-export default Search;
+  getInfo() {
+    // axios.get(`${API_URL}?api_key=${API_KEY}&prefix=${this.state.query}&limit=7`)
+    //   .then(({ data }) => {
+    //     this.setState({
+    //       results: data.data
+    //     })
+    //   })
+  }
+
+  handleInputChange() {
+    this.setState({
+      query: this.search.value
+    }, () => {
+      if (this.state.query && this.state.query.length > 1) {
+          this.getInfo();
+      } else if (!this.state.query) {
+      }
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          placeholder="Search for..."
+          ref={input => this.search = input}
+          onChange={this.handleInputChange}
+        />
+        <Suggestions results={this.state.results} />
+      </form>
+    )
+  }
+}
+
+export default Search
