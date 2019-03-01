7e90e21d4dd0ecea806a0979018917be5ba7ea21
David Anderson <dlande000@gmail.com>
2019-03-01 14:32:54 -0500

    Add search actions file

---
+++ frontend/actions/search_actions.js
@@ -0,0 +1,23 @@
+import * as SearchAPIUtil from '../util/search_api_util';
+
+export const RECEIVE_SEARCH_RESULTS = 'RECEIVE_SEARCH_RESULTS';
+export const CLEAR_SEARCH_RESULTS = 'CLEAR_SEARCH_RESULTS';
+
+function receiveSearchResults(users) {
+  return {
+    type: RECEIVE_SEARCH_RESULTS,
+    users
+  };
+}
+
+export const clearSearchResults = () => {
+  return {
+    type: CLEAR_SEARCH_RESULTS
+  };
+};
+
+export const fetchSearchResults = input => dispatch => {
+  return SearchAPIUtil.fetchSearchResults(input).then(users => {
+    return dispatch(receiveSearchResults(users));
+  });
+};
