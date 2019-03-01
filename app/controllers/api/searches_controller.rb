9e51174a41120af5834b652b9f3b5f9669b540d3
David Anderson <dlande000@gmail.com>
2019-03-01 17:02:00 -0500

    Update searches controller

---
+++ app/controllers/api/searches_controller.rb
@@ -0,0 +1,6 @@
+class Api::SearchesController < ApplicationController
+    def search
+        @users = User.where("lower(first_name) like :input or lower(last_name) like :input", input: "%#{params[:input].downcase}%")
+        render 'api/users/index'
+    end
+  end
