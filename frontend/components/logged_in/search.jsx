e841a5a873e19633da9d9f26ac2a7d7ca9b1a1cd
David Anderson <dlande000@gmail.com>
2019-03-01 13:54:36 -0500

    Write skeleton for search bar

---
+++ frontend/components/logged_in/search.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Search;
+
+export default Search;
---
+++ frontend/components/logged_in/search_container.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import Search from './search';
+
+const mapStateToProps = state => {
+};
+
+const mapDispatchToProps = dispatch => {
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Search);
