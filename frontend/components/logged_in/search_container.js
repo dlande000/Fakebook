6bf93186eaf3e80652310d9b851c7fa94b2268f2
David Anderson <dlande000@gmail.com>
2019-03-01 17:27:01 -0500

    Add search reducer to entities reducer, update search container

--- frontend/components/logged_in/search_container.js
+++ frontend/components/logged_in/search_container.js
@@ -1,13 +1,18 @@
 import { connect } from 'react-redux';
+import { fetchResults, clearSearch } from '../../actions/search_actions';
 import Search from './search';
 
 const mapStateToProps = state => {
+  return ({
+    results: state.entities.search
+  });
 };
 
 const mapDispatchToProps = dispatch => {
+  return {
+    fetchSearchResults: input => dispatch(fetchSearchResults(input)),
+    clearSearchResults: () => dispatch(clearSearchResults())
+  };
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Search);
+export default connect(mapStateToProps, mapDispatchToProps)(Search);
--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import postsReducer from './posts_reducer';
+import searchReducer from './search_reducer';
 
 const entitiesReducer = combineReducers({
   posts: postsReducer,
-  users: usersReducer
+  users: usersReducer,
+  search: searchReducer
 });
 
 export default entitiesReducer;
