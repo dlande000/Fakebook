3f0b569c52e4b1fa69f62334b204b7a165be33c1
David Anderson <dlande000@gmail.com>
2019-03-01 14:07:31 -0500

    Write constructor for search

--- frontend/components/logged_in/search.jsx
+++ frontend/components/logged_in/search.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
-const Search;
+class Search extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            searchTerm: '',
+            currentlyDisplayed: this.props.users
+        };
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+    }
+
+    render() {}
+}
 
 export default Search;
