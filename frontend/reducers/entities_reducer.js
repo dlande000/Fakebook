b30d86b7b73acdf77198629c966d571fbdc01525
David Anderson <dlande000@gmail.com>
2019-03-06 10:07:21 -0500

    Add comments reducer to entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import postsReducer from './posts_reducer';
 import searchReducer from './search_reducer';
+import commentsReducer from './comments_reducer';
 
 const entitiesReducer = combineReducers({
   posts: postsReducer,
   users: usersReducer,
-  search: searchReducer
+  search: searchReducer,
+  comments: commentsReducer
 });
 
 export default entitiesReducer;
