cb1050da3e368d61f5f58e3902b9962bf3634b1a
David Anderson <dlande000@gmail.com>
2019-03-06 09:51:45 -0500

    Add comments api util

---
+++ frontend/util/comments_api_util.js
@@ -0,0 +1,29 @@
+export const fetchComments = id => {
+    return $.ajax({
+      url: 'api/comments',
+      data: { post_id: id }
+    });
+  };
+  
+  export const createComment = comment => {
+    return $.ajax({
+      method: 'POST',
+      url: 'api/comments',
+      data: { comment }
+    });
+  };
+  
+  export const updateComment = comment => {
+    return $.ajax({
+      method: 'PATCH',
+      url: `api/comments/${comment.id}`,
+      data: { comment }
+    });
+  };
+  
+  export const deleteComment = id => {
+    return $.ajax({
+      method: 'DELETE',
+      url: `api/comments/${id}`
+    });
+  };
