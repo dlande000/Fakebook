db0900f4ef3b39a778c0ff7e0a81a3b9a1e10667
David Anderson <dlande000@gmail.com>
2019-03-09 15:05:22 -0500

    Add like icons posts

--- frontend/components/posts/post.jsx
+++ frontend/components/posts/post.jsx
@@ -3,6 +3,24 @@ import CommentsAllComponents from './comments_all_components';
 import { Link } from 'react-router-dom';
 
 class Post extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.likedIds = [];
+        this.props.post.likes.forEach(like => {
+            this.likedIds.push(like.userId);
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        if (this.likedIds.includes(this.props.currentUser.id)) {
+            this.props.deleteLike(postId);
+        } else {
+            this.props.createLike(postId);
+        }
+    }
+
     render() {
 
     const receiver = this.props.users[this.props.post.receiverId];
@@ -72,6 +90,20 @@ class Post extends React.Component {
         )
     }
 
+    let liked;
+    if (!this.likedIds.includes(this.props.currentUser.id)) {
+        liked = (
+            <div>
+                <a onClick={this.handleSubmit} href=""><img width="40px" src="https://image.flaticon.com/icons/png/512/39/39794.png" alt=""/> Like</a>
+            </div>
+        )
+    } else {
+        liked = (<div>
+            <a onClick={this.handleSubmit} href=""><img width="40px" src="https://cdn.designcrowd.com/blog/2016/March/icons-vs-logos/FBLike-Icon_300_v2.png" alt=""/> Like</a>
+        </div>
+        )
+    }
+
     return (
         <div className="post">
             <div className="post-image-container">
@@ -84,8 +116,21 @@ class Post extends React.Component {
             <div className="line-holder">
                 <hr className="post-horizontal-line"/>
             </div>
-            {likes}
-            {comments}
+            <div>
+                {likes}
+                {comments}
+            </div>
+            <div className="line-holder">
+                <hr className="post-horizontal-line"/>
+            </div>
+            <div>
+                <div>
+                    {liked}
+                </div>
+                <div>
+                    <a href=""><img width="40px" src="https://www.shareicon.net/download/2015/08/14/85004_comments_512x512.png" alt=""/> Comment</a>
+                </div>
+            </div>
             <div className="comments-all-components">
                 <CommentsAllComponents createComment={this.props.createComment} post={this.props.post} users={this.props.users} currentUser={this.props.currentUser}/>
             </div>
